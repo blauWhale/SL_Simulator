7397209d5b7c9a6fed4042914499d69b0557c443
Jonas Baars <60293072+jb-0001@users.noreply.github.com>
2022-01-11 09:46:16 +0100

    Updated Json structure, updated JsonReader

--- BlueSLSim/src/JsonReader.java
+++ BlueSLSim/src/JsonReader.java
@@ -3,11 +3,12 @@ import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
-import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
+
 
 /**
  * A JsonReader for a Teamsheet
@@ -22,20 +23,21 @@ public class JsonReader {
      */
     public ArrayList<Team> readJsonFile() {
         JSONParser jsonParser = new JSONParser();
-        try (FileReader reader = new FileReader("BlueSLSim/superLeagueTeams.json")) {
+        try (FileReader reader = new FileReader("BlueSLSim/json.json")) {
             //Read JSON file
             Object obj = jsonParser.parse(reader);
 
             JSONArray teamArray = (JSONArray) obj;
 
-            teamArray.forEach(team -> parseTeamObject((JSONObject) team));
-
+            parseJSONString(teamArray.toJSONString());
             return teamArrayList;
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         } catch (ParseException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
             e.printStackTrace();
         }
         return teamArrayList;
@@ -47,12 +49,29 @@ public class JsonReader {
      *
      * Adds the objects to an Arraylist
      *
-     * @param object JsonObject of anykind
+     * param object JsonObject of anykind
      */
-    public void parseTeamObject(JSONObject object) {
-        //Get employee object within list
-        JSONObject teamObject = (JSONObject) object.get("team");
-        Team team = new Team((String) teamObject.get("name"),new Rating(Integer.parseInt((String) teamObject.get("offensivRating")) ,Integer.parseInt((String) teamObject.get("defensivRating"))));
-        teamArrayList.add(team);
+    public static List parseJSONString(String jsonStr) throws Exception {
+        List<String> keyList = new ArrayList<String>();
+        JSONObject authorJsonObj = (JSONObject) new JSONParser().parse(jsonStr);
+
+        JSONArray teamsJsonArr = (JSONArray)authorJsonObj.get("teams");
+        for (Object o : teamsJsonArr) {
+            JSONObject teamsJsonObj = (JSONObject) o;
+            String isMulti = (String) teamsJsonObj.get("IsMulti");
+            String defensivRating = "" + teamsJsonObj.get("defensivRating");
+            String name = (String) teamsJsonObj.get("name");
+            String offensivRating = "" + teamsJsonObj.get("offensivRating");
+            JSONArray playersJsonArr = (JSONArray) teamsJsonObj.get("players");
+            for (Object value : playersJsonArr) {
+                JSONObject playersJsonObj = (JSONObject) value;
+                String isMultiOf = (String) playersJsonObj.get("IsMulti");
+                String defensivRatingOf = "" + playersJsonObj.get("defensivRating");
+                String nameOf = (String) playersJsonObj.get("name");
+                String offensivRatingOf = "" + playersJsonObj.get("offensivRating");
+                String position = (String) playersJsonObj.get("position");
+            }
+        }
+        return keyList;
     }
 }
